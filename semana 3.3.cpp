2e4ffa8cf15309c167cdbc09ed11bc60621c7517
KarlaSoberanis <136513349+KarlaSoberanis@users.noreply.github.com>
2023-06-20 15:03:18 -0600

    Add files via upload

---
+++ semana 3.3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+int main (){
+	
+	int i;
+	int i= nombre;
+	
+	char nombre [20];
+	
+	cout<<"ingrese el nombre de la persona: "<<endl;
+	cin.getline(nombre,20,'/n');
+	
+	cout<<nombre;
+	
+	while(i=a,e,i,o,u){
+		int distancia = strlen(nombre);
+		cout<<"cantidad de espacios"<<distancia;
+		
+		
+	}
+
+	
+	return 0;
+}
